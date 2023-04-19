1407f3eb2d3fea307be1efb51d7f233a0766d491
greentealatte2105 <20521775@ms.uit.edu.vn>
2023-04-19 17:04:25 +0700

    add code.

---
+++ src/main/java/org/example/FlyHigh.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class FlyHigh implements IFly{
+    @Override
+    public void Fly() {
+        System.out.println("Toi co the bay cao.");
+    }
+}
---
+++ src/main/java/org/example/FlyLow.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class FlyLow implements IFly{
+    @Override
+    public void Fly() {
+        System.out.println("Toi co the bay thap.");
+    }
+}
