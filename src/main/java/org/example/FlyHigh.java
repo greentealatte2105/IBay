2a6624c90b927c68d2f63db3b244f5a004193c76
greentealatte2105 <20521775@ms.uit.edu.vn>
2023-04-22 10:46:03 +0700

    modified src code --2.

--- src/main/java/org/example/FlyHigh.java
+++ src/main/java/org/example/FlyHigh.java
@@ -3,6 +3,6 @@ package org.example;
 public class FlyHigh implements IFly{
     @Override
     public void Fly() {
-        System.out.println("I can fly high (modified).");
+        System.out.println("I can fly high (modified --2).");
     }
 }
